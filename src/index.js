72d0494a56192e32b9fdf917f80f557c6333845b
Romamill <122994080+Romamill@users.noreply.github.com>
2023-07-20 09:21:29 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+const h1 = React.createElement("h1", {children: "Pisun"})
+console.log(h1)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {h1}
     <App />
   </React.StrictMode>
 );
