434d8cadeb0d8b10193c42583a369fde827c569f
Romamill <122994080+Romamill@users.noreply.github.com>
2023-07-25 19:17:08 +0300

    task1,2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import { Profile } from "./Profile/Profile";
+import user from '../data/user.json';
+import data from '../data/data.json';
+import {Statistics} from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +15,18 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+
     </div>
   );
 };
